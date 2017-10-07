0fbbfc521de9e28ae3406b4ecc24855ae5d06849
ellinzhao <ellin.zhao@hotmail.com>
2017-10-07 01:46:56 -0700

    get stops function added

--- ac_transit.js
+++ ac_transit.js
@@ -1,10 +1,15 @@
 //fed in location data
 
 var request = new XMLHttpRequest();
+var APIkey = "/?token=BB71819FE155622579B298B9C09BC87D";
 
-request.open("GET", "http://api.actransit.org/transit/servicenotices/?token=BB71819FE155622579B298B9C09BC87D", false);
+function getStops(lat, lon) {
+	request.open("GET", "http://api.actransit.org/transit/stops/" + lat + "/" + lon "/" + APIkey, false);
+	console.log(request.status);
+	console.log(request.statusText);
 
-var APIkey = "BB71819FE155622579B298B9C09BC87D";
+}
 
-console.log(request.status);
-console.log(request.statusText);
+
+
+
