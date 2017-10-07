2ce5ada0af0b19387ff84ce50ba019809fbd6624
Avery Yip <averyyip@ymail.com>
2017-10-07 14:42:10 -0700

    Changed request module

--- ac_transit.js
+++ ac_transit.js
@@ -1,14 +1,15 @@
 //fed in location data
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var request = new XMLHttpRequest();
+var request = require('request');
 
 var APIkey = "/?token=BB71819FE155622579B298B9C09BC87D";
 
 var actransit = {
 	getStops: function(lat, lon) {
-		request.open("GET", "http://api.actransit.org/transit/stops/" + lat + "/" + lon + "/" + "500" + "/" + APIkey, false);
-		console.log(request.response);
-		console.log(request.responseText);
+		request("http://api.actransit.org/transit/stops/" + lat + "/" + lon + "/" + "500" + "/" + APIkey, function (error, response, body) {
+		  console.log('error:', error) // Print the error if one occurred
+		  console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
+		  console.log('body:', body) // Print the HTML for the Google homepage.
+		})
 	},
 
 	getPredictions: function(stopid) {
@@ -27,4 +28,5 @@ var actransit = {
 }
 
 module.exports = actransit
+
 
