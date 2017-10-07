25780c70e293efb8507a196e28d8e2a594a1c934
Wen Rui <wenrui@berkeley.edu>
2017-10-07 02:34:24 -0700

    Started on GetPredictions from AC Transit API

--- ac_transit.js
+++ ac_transit.js
@@ -7,9 +7,16 @@ function getStops(lat, lon) {
 	request.open("GET", "http://api.actransit.org/transit/stops/" + lat + "/" + lon "/" + APIkey, false);
 	console.log(request.status);
 	console.log(request.statusText);
-
 }
 
+function getPredictions(stopid) {
+	request.open("GET", "http://api.actransit.org/transit/stops/" + stopid + "/predictions/" + APIkey, false);
+	//Gives all predictions
+	console.log(request.status);
+	console.log(request.statusText);
+}
 
-
-
+function getBusPredictions(stopid, bus_number) {
+	var predictions = getPredictions(stopid);
+	//Sieve and cull the unnecessary routes
+}
